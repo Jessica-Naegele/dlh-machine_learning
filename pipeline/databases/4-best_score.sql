4fe0b377c3f42b8d2bb1e15fa870b3b7e6e56f2a
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-09 19:40:55 +0200

    3-5-4 it 2

--- pipeline/databases/4-best_score.sql
+++ pipeline/databases/4-best_score.sql
@@ -1,10 +1,5 @@
 -- selects only those with score > 10
 
-<<<<<<< HEAD
 SELECT score, name FROM second_table
 WHERE score >= 10
 ORDER BY score DESC; 
-=======
-SELECT * FROM second_table
-WHERE score > 10;
->>>>>>> 8de40e1f6af2e3771b18a9502afcdd896d08a5a1
