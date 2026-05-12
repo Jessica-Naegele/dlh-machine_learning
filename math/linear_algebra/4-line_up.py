7cb409301b60edbf33ef30bf670d4ba25c45973a
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-12 16:51:33 +0200

    4-0-4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,21 +4,9 @@
 def add_arrays(arr1, arr2):
     """Function to add two arrays element wise"""
     new_l = []
-    size1 = []
-    current_layer = arr1
-    while isinstance(current_layer, list):
-        size1.append(len(current_layer))
-        current_layer = current_layer[0]
-    size2 = []
-    current_layer = arr1
-    while isinstance(current_layer, list):
-        size2.append(len(current_layer))
-        current_layer = current_layer[0]
-    if size1 == size2:
-        for i in range(0, size1-1):
+    if len(arr1) == len(arr2):
+        for i in range(0, len(arr1)):
             new_l.append(arr1[i]+arr2[i])
         return new_l
     else:
         return None
-
-
