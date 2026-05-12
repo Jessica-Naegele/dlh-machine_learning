63b1d55bb6029b767f36b75693663f5c809373cd
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-12 16:47:47 +0200

    4-0-4

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,5 +1,5 @@
 -- display max temperatures per states
 
-SELECT state, MAX(value) as max_temp FROM hbtn_0c_0.temperatures
+SELECT state, MAX(value) as max_temp FROM temperatures
 GROUP BY state
-ORDER BY state;
+ORDER BY state ASC;
