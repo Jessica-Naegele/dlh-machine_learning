3bf01a5f4902ae79f896d2b46de26e745adf8104
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-12 15:14:00 +0200

    3-5-6

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- displayes avg temperatures by city orderd by temperatures
 
-SELECT city, avg(value) AS avg_temp FROM hbtn_0c_0.temperatures
+SELECT city, avg(value) AS avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
