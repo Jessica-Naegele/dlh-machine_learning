2f0a81af8e69a975736fa64356e24408c67a08ff
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-12 17:09:01 +0200

    4-0-5 - it2

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,23 +3,14 @@
 
 
 def add_matrices2D(mat1, mat2):
-    size1 = []
-    current_layer = mat1
+    """someone forgot the comment to describe the function"""
     new_m = []
-    while isinstance(current_layer, list):
-        size1.append(len(current_layer))
-        current_layer = current_layer[0]
-    size2 = []
-    current_layer = mat2
-    while isinstance(current_layer, list):
-        size2.append(len(current_layer))
-        current_layer = current_layer[0]
-    if size1 == [2, 2] and size2 == [2, 2]:
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    else:
         for i in range(0, len(mat1)):
             r = []
             for j in range(0, len(mat1[i])):
                 r.append(mat1[i][j] + mat2[i][j])
             new_m.append(r)
         return new_m
-    else:
-        return None
