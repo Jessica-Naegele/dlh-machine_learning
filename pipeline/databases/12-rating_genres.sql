a94bf9003e039007176526894493c060056e961b
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-13 20:24:32 +0200

    3-5-12

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -4,4 +4,4 @@ SELECT name, SUM(rate) AS rating FROM tv_genres t1
 JOIN tv_show_genres t2 ON t1.id = t2.genre_id
 JOIN tv_show_ratings t3 ON t2.show_id = t3.show_id
 GROUP BY name
-ORDER BY rating desc;
+ORDER BY rating DESC;
