e6ffc7ed1ccd3bfebeba87fadea1d0ad82736b2e
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-13 20:24:02 +0200

    3-5-12

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,7 +1,7 @@
 -- script lists all genres in database by rating
 
-SELECT name, SUM(rate) as rating FROM tv_genres t1
+SELECT name, SUM(rate) AS rating FROM tv_genres t1
 JOIN tv_show_genres t2 ON t1.id = t2.genre_id
-JOIN tv_show_ratings t3 on t2.show_id = t3.show_id
+JOIN tv_show_ratings t3 ON t2.show_id = t3.show_id
 GROUP BY name
 ORDER BY rating desc;
