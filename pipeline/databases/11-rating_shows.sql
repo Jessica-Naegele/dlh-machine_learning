5fce976bbaeb569d867b1a74e042fcad2acd9b57
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-13 20:15:33 +0200

    3-5-11

---
+++ pipeline/databases/11-rating_shows.sql
@@ -0,0 +1,9 @@
+-- Active: 1778347204990@@localhost@3306@hbtn_0d_tvshows_rates
+-- script that lists all shows by their rating
+
+SELECT title, SUM(rate) AS rating FROM tv_shows t1
+JOIN tv_show_ratings t2 ON t1.id = t2.show_id
+GROUP BY title
+ORDER BY rating DESC;
+
+
