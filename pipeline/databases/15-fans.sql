7fc0e9018b9361535081a1f4cd14985bdd6f76a9
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-13 21:14:33 +0200

    3-5-15

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,4 +1,4 @@
---- select origins of bands ordered by number of non-unique fans
+-- select origins of bands ordered by number of non-unique fans
 
 SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin
