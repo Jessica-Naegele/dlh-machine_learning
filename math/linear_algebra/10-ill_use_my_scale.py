45e316c549d93e0b970202e50bbc52cec78f4297
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-13 16:14:01 +0200

    4-0-10

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Write a function def np_shape(matrix): that calculates the shape of a numpy.ndarray:
+"""Write a function def np_shape(matrix): 
+that calculates the shape of a numpy.ndarray:
 
 You are not allowed to use any loops or conditional statements
 You are not allowed to use try/except statements
