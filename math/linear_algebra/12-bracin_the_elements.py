42f32706149a84485ef66ce419e4e7f6b95a282c
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-14 13:14:46 +0200

    4-0-12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Function perfoming element-wise addition, subtraction, 
+"""Function perfoming element-wise addition, subtraction,
 multiplicaiton and division
 You can assume that mat1 and mat2 can be interpreted as numpy.ndarrays
-You should return a tuple containing the element-wise sum, 
+You should return a tuple containing the element-wise sum,
 difference, product, and quotient, respectively
 You are not allowed to use any loops or conditional statements
 You can assume that mat1 and mat2 are never empty
