3f834e86c9dccb58be0688c5f6a979b0d861e21f
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-14 12:50:24 +0200

    4-0-12

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -10,6 +10,5 @@ import numpy as np
 
 def np_transpose(matrix):
     """function to transpose a matrix with np"""
-    new_mat = np.array(matrix())
+    new_mat = np.array(matrix)
     return new_mat.transpose()
-
