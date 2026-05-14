df23c0c0ba368e77e9279b526c0b9b03e55c093c
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-14 20:35:19 +0200

    3-5-17

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,10 @@
+-- sql script that creates a trigger that decreases the quanitiy of an item ater adding a new order
+
+CREATE TRIGGER IF NOT EXISTS test
+AFTER INSERT ON orders
+FOR EACH ROW 
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
