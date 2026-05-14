96dd3b3dd632a6efb604085f91d840681d820392
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-14 17:17:55 +0200

    4-0-15

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -4,13 +4,6 @@
 
 def np_slice(matrix, axes={}):
     """function slicing a matrix"""
-    """ axes={1: (1, 3)}
-        1: key 
-        (1,3) -  
-        print(arr[1, 1:4])
-        axes={0: (2,), 2: (None, None, -2)})
-        print(arr[0:2, 1:4])
-    """
     selection = [slice(None)] * matrix.ndim
     for axis, values in axes.items():
         selection[axis] = slice(*values)
