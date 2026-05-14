bf58c7bfe7ca463f8b5ae726d800f12c1d6fa93a
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-14 12:47:30 +0200

    4-0-11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" function that transposes a matrix
+You can assume that matrix can be interpreted as a numpy.ndarray
+You are not allowed to use any loops or conditional statements
+You must return a new numpy.ndarray
+"""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """function to transpose a matrix with np"""
+    new_mat = np.array(matrix())
+    return new_mat.transpose()
+
