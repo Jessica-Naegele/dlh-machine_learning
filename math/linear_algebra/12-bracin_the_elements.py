e1a49c72a5e3dd599217118a35965bca1e11ced0
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-14 13:19:06 +0200

    4-0-12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -8,19 +8,17 @@ You are not allowed to use any loops or conditional statements
 You can assume that mat1 and mat2 are never empty
 """
 
-import numpy as np
-
 
 def np_elementwise(mat1, mat2):
     """Function returning add sub mul div"""
     tp = []
-    add = np.add(mat1, mat2)
+    add = mat1 + mat2
     tp.append(add)
-    sub = np.subtract(mat1, mat2)
+    sub = mat1 - mat2
     tp.append(sub)
-    mul = np.multiply(mat1, mat2)
+    mul = mat1 + mat2
     tp.append(mul)
-    div = np.divide(mat1, mat2)
+    div = mat1 / mat2
     tp.append(div)
 
     tp = tuple(tp)
