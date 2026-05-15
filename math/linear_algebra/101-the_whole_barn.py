5ec62a55b9f81b5b8b16361ad14401087b226f95
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-15 16:05:19 +0200

    4-0-101

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -11,11 +11,13 @@ def matrix_shape(matrix):
         current_layer = current_layer[0]
     return size
 
+
 def add_recursive(item1, item2):
     """Recursively drills down to add elements at the lowest layer."""
     if not isinstance(item1, list):
         return item1 + item2
     return [add_recursive(sub1, sub2) for sub1, sub2 in zip(item1, item2)]
+
 
 def add_matrices(mat1, mat2):
     """Adds two matrices of any dimension if they have the same shape."""
