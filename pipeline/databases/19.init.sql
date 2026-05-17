6dc04681034da1cea68e18c06ffb4aadc3627729
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-17 15:28:23 +0200

    3-5-21

--- pipeline/databases/19.init.sql
+++ pipeline/databases/19.init.sql
@@ -26,6 +26,7 @@ CREATE TABLE IF NOT EXISTS corrections (
     CONSTRAINT fk_project_id FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE
 );
 
+select * from corrections;
 INSERT INTO users (name) VALUES ("Bob");
 SET @user_bob = LAST_INSERT_ID();
 
--- pipeline/databases/20-main.sql
+++ pipeline/databases/20-main.sql
@@ -1,6 +1,8 @@
 SELECT * FROM users;
 SELECT * FROM corrections;
 
+#drop Procedure ComputeAverageScoreForUser;
+
 SELECT "--";
 CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
 
