bdd7b75a8eead9e04c8370750174fa934ecaf117
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-21 16:58:37 +0200

    3,5,102

---
+++ pipeline/databases/102-need_meeting.sql
@@ -0,0 +1,7 @@
+-- create a view
+
+CREATE OR REPLACE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 
+  AND (last_meeting IS NULL OR TIMESTAMPDIFF(MONTH, last_meeting, CURDATE()) >= 1);
