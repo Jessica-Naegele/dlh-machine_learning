6fd7a9a27171851ac37557fb677f7aa199405396
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-02 16:39:58 +0200

    6-0-9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Writing a function that calculates summation i sqaured"""
+
+
+def recursion(i, n):
+    """recursion for the calculation"""
+    # Base Case: n and i are equal
+    if i == n:
+        return n * n
+    else:
+        return i * i + recursion(i+1, n)
+
+
+
+def summation_i_squared(n):
+    """Function to calculate summation of i squared"""
+    if not isinstance(n, int):
+        return None
+    return recursion(0, n)
