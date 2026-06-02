fe0f0f68304ad065ff632640f78ddd57297a3ce6
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-02 18:26:34 +0200

    6-0-9 it 5

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,10 +4,6 @@
 
 def summation_i_squared(n):
     """Calculates the sum of i squared from 1 to n"""
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 1:
         return None
-    if n < 0:
-        return None
-    if n == 0:
-        return 0
-    return int((n * (n + 1) * (2 * n + 1)) // 6)
+    return n * (n + 1) * (2 * n + 1) // 6
