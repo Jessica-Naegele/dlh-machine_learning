0835f9c1c6923d8ea74f2edb20af1e8b2da604e4
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-02 18:38:23 +0200

    6-0-10 it 2

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -19,9 +19,5 @@ def poly_derivative(poly):
 
     h_poly = poly[:]
 
-    for i in range(le):
-        d = h_poly[0] * i
-        h_poly = h_poly[1:]
-        r.append(d)
-    r = r[1:]
+    r = [i * poly[i] for i in range(1, le)]
     return r
