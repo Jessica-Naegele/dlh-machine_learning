78b03041a8858788cf613aeb66706c7e035fb442
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-03 13:35:12 +0200

    6-1-0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(np.arange(0, 11), y, color='red')
+    plt.xlim(0, 10)  # limits the x achsis
+    plt.show()
+
