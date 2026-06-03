e5d2f60f17c2e0efff2bfc7ec754c281d6b425e3
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-03 13:36:47 +0200

    6-1-0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,7 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """create a red line"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
@@ -10,4 +12,3 @@ def line():
     plt.plot(np.arange(0, 11), y, color='red')
     plt.xlim(0, 10)  # limits the x achsis
     plt.show()
-
