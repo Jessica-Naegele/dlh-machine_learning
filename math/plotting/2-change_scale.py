60f696fd4928868acc1de84950bb230314c2cab9
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-03 14:16:47 +0200

    6-1-2

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Complete the following source code to plot x ↦ y as a line graph:
+The x-axis should be labeled Time (years)
+The y-axis should be labeled Fraction Remaining
+The title should be Exponential Decay of C-14
+The y-axis should be logarithmically scaled
+The x-axis should range from 0 to 28650
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def change_scale():
+    """labeling, title, scaling"""
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.plot(x, y, color="blue")
+    plt.title("Exponential Decay of C-14")
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.xlim(0, 28650)  
+    plt.yscale('log')  # log scaling of the y achsis
