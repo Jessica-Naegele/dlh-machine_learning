4ab44e1605bd616524984761e1a40b10b60951a3
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-04 17:27:56 +0200

    6-1-100

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""creating a gradiant scatter"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """This scatter is a gradiant"""
+
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    colors = np.random.rand(100)
+    plt.scatter(x, y, c=colors)
+    plt.title("Mountain Elevation")
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.zlabel('elevation (m)')  # z gibt es nicht
+    # plt.xlim(0, 10)  # limits the x achsis
+    plt.show()
