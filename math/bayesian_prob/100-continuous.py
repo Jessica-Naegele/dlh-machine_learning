1dfc8522427ce4a013873a32bd182489be4b0225
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-23 09:41:25 +0200

    8-0-100 no numpy

--- math/bayesian_prob/100-continuous.py
+++ math/bayesian_prob/100-continuous.py
@@ -3,68 +3,7 @@
 calculating the marginal probability
 """
 
-import numpy as np
 import scipy as sc
-
-
-def faculty(z):
-    """this is a sub function to create the faculty"""
-    t = 1
-    for i in range(1, z+1):
-        t *= i
-    return t
-
-
-def likelihood(x, n, P):
-    """
-    Function for calculating the likelihood
-    applying bayseian & binominal distribution
-    """
-    # Sanity Checks
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError(
-            "x must be an integer that is greater than or equal to 0"
-            )
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    for i in P:
-        if (i < 0 or i > 1):
-            raise ValueError(
-                "All values in P must be in the range [0, 1]"
-                )
-    # actual code
-    Pp = np.empty(len(P))  # used for storing the result
-    # binom formula (n! / (x! (n-x)!) * p ** x * (1-p) ** (n-x))
-    combination_factor = faculty(n) / (faculty(x) * (faculty(n-x)))
-    Pp = np.array(list(map(
-        lambda v: combination_factor * (v ** x) * ((1 - v) ** (n - x)),
-        P
-        )))
-    return Pp
-
-
-def intersection(x, n, P, Pr):
-    """
-    Function to calculate intersection of obtaining this data with
-    various hypothetical probabilities
-    """
-    # Intersection = Likelihood * Prior
-    ll = likelihood(x, n, P)
-    inter = np.multiply(Pr, ll)
-    return inter
-
-
-def marginal(x, n, P, Pr):
-    """
-    function calculating the marginal probability
-    """
-    # Marginal Probabiltiy = Sum Intersection
-    marge = np.sum(intersection(x, n, P, Pr))
-    return marge
 
 
 def posterior(x, n, p1, p2):
