dc51183cc437a900d1c9ac4168bb92a763b22199
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-24 15:49:28 +0200

    9-0-1 change order of validations

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -12,7 +12,7 @@ def correlation(C):
     """
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1] or len(C.shape) != 2:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     # Step 1: find variances
     var = []
