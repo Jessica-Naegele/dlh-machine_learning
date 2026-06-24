7358d16161e855bd01015226dfa6a7623c82e6fa
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-24 11:51:09 +0200

    9-0-02

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -8,7 +8,7 @@ def mean_cov(X):
     """Function that calculates the mean and covariance ofdata set
     X is a numpy.ndarray with shape (n, d)
     n = number of data points
-    d = number of dimension in each data point    
+    d = number of dimension in each data point
     """
     # if X is not a 2D numpy.ndarray raise Typerror
     if not isinstance(X, np.ndarray) or len(np.shape(X)) != 2:
@@ -17,14 +17,13 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     n = X.shape[0]
     d = X.shape[1]
-    
+
     # print(f"n: {n}")  # helper
     # print(f"d: {d}")  # helper 
 
     # if n is less than 2
-    if n < 2: 
+    if n < 2:
         raise ValueError("X must contain multiple data points")
-    
     # mean = numpy.ndarray of shape (1, d)
     mean = np.mean(X, axis=0, keepdims=True)
     # print(f"mean: {mean}") #helper
