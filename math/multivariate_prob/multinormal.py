6373727c4536d187c42b7f935c8437ca0b5b68a3
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-06-25 11:29:26 +0200

    9-0-2

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -8,7 +8,7 @@ class MultiNormal():
     """Class representing a Multivariate Normal Distribution"""
 
     def __init__(self, data):
-        """Initiate the class 
+        """Initiate the class
         data is a numpy.ndarray of shape (d, n)
         as mean and cov are public instance variable,
         they all need to be set in __init__
@@ -20,12 +20,16 @@ class MultiNormal():
             raise ValueError("data must contain multiple data points")
         else:
             self.data = data
-        
+
         # set mean
         self.mean = np.mean(data, axis=1, keepdims=True)
 
-        #set cov (does it work the same way?)
+        # set cov (does it work the same way?)
         # Step 1 create a centered matrix
         DataC = data - self.mean
-        # Step 2 Multiplication (d, n) - (d, n) * (n, d)) (result should be d, d)
-        self.cov = (np.matmul(DataC, np.matrix.transpose(DataC))) / (data.shape[1] - 1)
+        # Step 2 Multiplication (d, n) - (d, n)
+        # * (n, d)) (result should be d, d)
+        self.cov = (
+            (np.matmul(DataC, np.matrix.transpose(DataC)))
+            / (data.shape[1] - 1)
+        )
