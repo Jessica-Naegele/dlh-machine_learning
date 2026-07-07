a239261503784bbb712cc676649cf4fa3f21f6b9
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-07 15:47:38 +0200

    9-0-0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -2,7 +2,7 @@
 """ function creating a data from from a np ndarray"""
 
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 
 def from_numpy(array):
@@ -11,16 +11,14 @@ def from_numpy(array):
     dictionary keys = column labels and values = column values
     """
     s = array.shape
-    # print(f"s: {s}") #helper #A s = 5,8 --> 5 rows 8 columns
+    # print(f"s: {s}")  # A s = 5,8 --> 5 rows 8 columns
     r_in = s[0]
     c_in = s[1]
-    # print(f"r_in: {r_in}, c_in: {c_in}") #helper
-    c_range = list(chr(65 + i) for i in range(c_in)) 
+    # print(f"r_in: {r_in}, c_in: {c_in}")
+    c_range = list(chr(65 + i) for i in range(c_in))
     r_range = np.arange(0, r_in)
-    # print(f"c_range: {c_range}") #helper
-    # print(f"r_range: {r_range}") #helper
+    # print(f"c_range: {c_range}")
+    # print(f"r_range: {r_range}")
 
     df = pd.DataFrame(array, index=np.arange(0, r_in), columns=c_range)
     return df
-
-
