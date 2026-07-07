17056bfd9e22002bff7cd6c98d88a275900d03b2
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-07 16:08:24 +0200

    9-0-1

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" creating a script defining dp"""
+
+import pandas as pd
+
+
+data = {
+      "First": [0.0, 0.5, 1.0, 1.50],
+      "Second": ["one", "two", "three", "four"]
+    }
+df = pd.DataFrame(data, index = ["A", "B", "C", "D"])
