3438f240a2bbda6b2038e8af088ccb124a46440c
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-08 15:46:29 +0200

    9-0-4

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""function that selects last 10 rows and ocnverst the selected values in a numpy value"""
+"""function that selects last 10 rows and ocnverst the selected
+values in a numpy value"""
 
 import pandas as pd
 
 
 def array(df):
-    """function 
+    """function
     - selecting last 10 rows of High and Close
     Convert them into a numpy ndarray"""
-    df_select = df["High", "Close"].tail(10)
+    df_select = df[['High', 'Close']].tail(10)
     return df_select.to_numpy()
