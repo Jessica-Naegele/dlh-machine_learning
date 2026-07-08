644adf41197e627d752488af4238c88f8a1f5ee2
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-08 15:42:11 +0200

    9-0-4

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that selects last 10 rows and ocnverst the selected values in a numpy value"""
+
+import pandas as pd
+
+
+def array(df):
+    """function 
+    - selecting last 10 rows of High and Close
+    Convert them into a numpy ndarray"""
+    df_select = df["High", "Close"].tail(10)
+    return df_select.to_numpy()
