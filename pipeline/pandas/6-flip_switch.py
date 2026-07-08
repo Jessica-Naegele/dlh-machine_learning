2d9544037ff83ba75e79e1bec5efbb1ce9874f13
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-08 16:04:54 +0200

    9-0-6 no importing pandas

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """sort data in reverse chornological order and transposes sorted dataframe"""
 
-import pandas as pd
-
 
 def flip_switch(df):
     """transpose and sort a dataframe"""
